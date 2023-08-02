6388807b9fab2e65d956cdfb0c9893fc0c7dbfa6
Harry Day <h-day@outlook.com>
2023-08-02 17:02:26 +0100

    Adding ability to grab book data

--- Introduction/bookscraper/bookscraper/spiders/bookspider.py
+++ Introduction/bookscraper/bookscraper/spiders/bookspider.py
@@ -1,6 +1,7 @@
 import scrapy
-
-import pudb
+# from scrapy import Spider
+# from scrapy import Request
+# from scrapy.crawler import CrawlerProcess
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
@@ -25,3 +26,28 @@ class BookspiderSpider(scrapy.Spider):
             else:
                 next_page_url = "https://books.toscrape.com/catalogue/" + next_page
             yield response.follow(next_page_url, callback=self.parse)
+
+    
+    def parse_book_page(self, response):
+    
+        table_rows = response.css('table tr')  # Getting the table information for a given book
+
+        yield {
+            'url': response.url,
+            'title': response.css('.product_main h1::text').get(),
+            'product_type': table_rows[1].css("td ::text").get(),
+            'price_excl_tax': table_rows[2].css("td ::text").get(),
+            'price_incl_tax': table_rows[3].css("td ::text").get(),
+            'tax': table_rows[4].css("td ::text").get(),
+            'availability': table_rows[5].css("td ::text").get(),
+            'num_reviews': table_rows[6].css("td ::text").get(),
+            'stars': response.css("p.star-rating").attrib['class'],
+            'category': response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+            'description': response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+            'price': response.css('p.price_color ::text').get(),
+        }
+
+# if __name__ == "__main__":
+#     process = CrawlerProcess()
+#     process.crawl(BookspiderSpider)
+#     process.start()
