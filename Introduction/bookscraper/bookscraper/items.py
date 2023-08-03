527deaff76581115e15d5a07f64c5992dd31a298
Harry Day <h-day@outlook.com>
2023-08-03 10:56:55 +0100

    Addition of pipeline to preprocess data before entering a csv/json - json included

--- Introduction/bookscraper/bookscraper/items.py
+++ Introduction/bookscraper/bookscraper/items.py
@@ -1,4 +1,5 @@
 # Define here the models for your scraped items
+# Own Notes: Items help us define what is in the block of data we're scraping
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
@@ -10,3 +11,24 @@ class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+def serialize_price(value):
+    return f'£ {str(value)}'  # Adds a £-sign to field it's added to below
+
+
+
+class BookItem(scrapy.Item):
+   url = scrapy.Field()
+   title = scrapy.Field()
+   upc = scrapy.Field()
+   product_type = scrapy.Field()
+   price_excl_tax = scrapy.Field()
+   price_incl_tax = scrapy.Field()
+   tax = scrapy.Field()
+   availability = scrapy.Field()
+   num_reviews = scrapy.Field()
+   stars = scrapy.Field()
+   category = scrapy.Field()
+   description = scrapy.Field()
+   price = scrapy.Field()
